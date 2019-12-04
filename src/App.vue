ab53f0b0f90935e83b0907d7cb71c64d8a65c3f7
JackieBinya <jacquelinebinya@gmail.com>
2019-12-05 01:17:23 +0200

    Basic set up of project
    - Bootstrapped the project with Vue CLI
    - Adds Google icons

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Shopping List App</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+
+}
+</script>
+
+<style>
+ @import url('https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap');
+ #app{
+   font-family: 'Montserrat', sans-serif;
+ }
+</style>
